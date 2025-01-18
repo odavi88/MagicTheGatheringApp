4394dd07e5c52be13755c9aa86f2a74e8a00baa1
Omar Davidson <omardavidson227@gmail.com>
2025-01-18 15:38:22 -0500

    building out the CardList component; passing CardList component into App component

---
+++ mtg-app/src/components/Card.tsx
@@ -0,0 +1,11 @@
+// This should be where the api call is happening
+
+function Card() {
+    return(
+        <>
+            
+        </>
+    );
+};
+
+export default Card;
---
+++ mtg-app/src/components/CardList.tsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function CardList() {
+    return(
+       <ul>
+            <li> <Card /> </li>
+       </ul>
+    );
+};
+
+export default CardList;
